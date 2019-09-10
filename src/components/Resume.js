c321be5d3e5ba775d9fe9693d9c93b62bb2eb3df
Ali Reubenstone <a.stone1794@gmail.com>
2019-09-10 12:47:00 -0400

    happy with resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import styled from 'styled-components'
 
+// import React from "react";
+// import resume from "../assets/resume.pdf";
+
+// const Resume = () => {
+//   return (
+//     <div className="resume wrapper" data-aos="fade-up">
+//       <object type="application/pdf" data={resume} title="resume" />
+//     </div>
+//   );
+// };
+//
+// export default Resume;
+
 const Resume = () => {
 
   const ViewResume = styled.div`
