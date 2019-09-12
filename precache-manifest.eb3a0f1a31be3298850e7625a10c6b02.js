2a01a918cabb84bc3b707fac2aa5d2e7fd614dde
Ali Reubenstone <a.stone1794@gmail.com>
2019-09-12 13:01:45 -0400

    Updates

---
+++ precache-manifest.eb3a0f1a31be3298850e7625a10c6b02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83e82139d3cbd481e773442e1784018",
+    "url": "/acting-portfolio/index.html"
+  },
+  {
+    "revision": "ccc54b45f7d189d6e02f",
+    "url": "/acting-portfolio/static/css/main.375b5edf.chunk.css"
+  },
+  {
+    "revision": "27f806dfd1833cffd092",
+    "url": "/acting-portfolio/static/js/2.daeb3917.chunk.js"
+  },
+  {
+    "revision": "ccc54b45f7d189d6e02f",
+    "url": "/acting-portfolio/static/js/main.068c0c1c.chunk.js"
+  },
+  {
+    "revision": "375c06414e8b26bec3d7",
+    "url": "/acting-portfolio/static/js/runtime~main.32bda46b.js"
+  }
+]);
